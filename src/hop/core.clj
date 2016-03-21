58d8d5accd9362b58342b1a8c437ac0228e1078d
James Reeves <jreeves@weavejester.com>
2016-03-21 03:27:29 +0000

    Add Maven dependencies to classpath function

--- src/hop/core.clj
+++ src/hop/core.clj
@@ -1,9 +1,24 @@
 (ns hop.core
-  (:require [clojure.string :as str]
+  (:require [cemerick.pomegranate.aether :as aether]
+            [clojure.string :as str]
             [clojure.java.io :as io]))
 
 (defn- absolute-path [path]
   (.getAbsolutePath (io/file path)))
 
-(defn classpath [{:keys [directories]}]
-  (str/join java.io.File/pathSeparator (map absolute-path directories)))
+(defn- classpath-dirs [{:keys [directories]}]
+  (map absolute-path directories))
+
+(def default-repositories
+  (merge aether/maven-central {"clojars" "http://clojars.org/repo"}))
+
+(defn- classpath-deps [{:keys [dependencies]}]
+  (when dependencies
+    (aether/dependency-files
+     (aether/resolve-dependencies
+      :coordinates  dependencies
+      :repositories default-repositories))))
+
+(defn classpath [project]
+  (->> (concat (classpath-dirs project) (classpath-deps project))
+       (str/join java.io.File/pathSeparator)))
