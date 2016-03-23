4f91eed1db25d7a1f82563a6aad07f1180e662b6
James Reeves <jreeves@weavejester.com>
2016-03-23 17:17:02 +0000

    Add 'version' task

--- build.clj
+++ build.clj
@@ -2,4 +2,5 @@
  :dependencies [[org.clojure/clojure "1.8.0"]
                 [com.cemerick/pomegranate "0.3.0"]
                 [meta-merge "0.1.1"]]
- :tasks {"refresh" {:main hop.core}}}
+ :tasks {"refresh" {:main hop.core}
+         "version" {:main hop.version}}}
---
+++ src/hop/version.clj
@@ -0,0 +1,5 @@
+(ns hop.version
+  (:gen-class))
+
+(defn -main [& args]
+  (println "Hop 0.1.0-SNAPSHOT"))
