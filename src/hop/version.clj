4410d643df7a22299984cf8287ad53679215e869
James Reeves <jreeves@weavejester.com>
2016-03-23 19:14:33 +0000

    Improve version task output

--- src/hop/version.clj
+++ src/hop/version.clj
@@ -1,5 +1,10 @@
 (ns hop.version
   (:gen-class))
 
+(def version
+  "0.1.0-SNAPSHOT")
+
 (defn -main [& args]
-  (println "Hop 0.1.0-SNAPSHOT"))
+  (println "Hop" version "on Java"
+           (System/getProperty "java.version")
+           (System/getProperty "java.vm.name")))
