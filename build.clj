c1bbbe4d1736df0cb8c6a6c356e390b5d85cf217
James Reeves <jreeves@weavejester.com>
2016-03-26 21:08:24 +0000

    Add compilation to build.clj

--- build.clj
+++ build.clj
@@ -2,10 +2,11 @@
  :group "hop"
  :version "0.1.0-SNAPSHOT"
  :source-paths ["src"]
+ :compile-path "target/classes"
  :dependencies [[org.clojure/clojure "1.8.0"]
                 [com.cemerick/pomegranate "0.3.0"]
                 [meta-merge "0.1.1"]
                 [medley "0.7.3"]
                 [leiningen "2.6.1"]]
  :tasks {"refresh" {:main hop.core}
-         "lein"    {:main hop.leiningen, :args [~task]}}}
+         "lein"    {:main hop.leiningen, :args [~task], :aot :all}}}
