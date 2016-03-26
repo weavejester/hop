ffa88a6c9a7ca95813cc4692fac0b1c9a8d26686
James Reeves <jreeves@weavejester.com>
2016-03-26 21:08:01 +0000

    Improve Leiningen eval-in-project efficienty

--- src/hop/leiningen.clj
+++ src/hop/leiningen.clj
@@ -1,15 +1,19 @@
 (ns hop.leiningen
   (:gen-class)
   (:require [clojure.java.io :as io]
+            [leiningen.core.eval :as eval]
             [leiningen.core.main :as main]
             [leiningen.core.project :as project]))
+
+(defmethod eval/eval-in :hop [_ form]
+  (eval form))
 
 (def root-path
   (.getAbsolutePath (io/file ".")))
 
 (defn- lein-project [build]
   (-> build
-      (assoc :root root-path, :plugins [], :eval-in-leiningen true)
+      (assoc :root root-path, :plugins [], :eval-in :hop)
       (dissoc :args :directories :main :repositories)
       (project/make)
       (project/init-project)))
