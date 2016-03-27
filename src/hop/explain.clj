00f2adca386e2e512e4156ad609f908cb513b5f0
James Reeves <jreeves@weavejester.com>
2016-03-27 05:08:04 +0100

    Change 'explain' to explain tasks, not build

--- src/hop/explain.clj
+++ src/hop/explain.clj
@@ -2,5 +2,8 @@
   (:gen-class)
   (:require [clojure.pprint :as pp]))
 
-(defn -main [build & args]
-  (pp/pprint (read-string build)))
+(defn -main [raw-build task-name & args]
+  (let [build (read-string raw-build)]
+    (if-let [task (get-in build [:tasks task-name])]
+      (pp/pprint task)
+      (println "No such task:" task-name))))
