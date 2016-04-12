8abb09451a1bfb71272d17f779cf124fcbe3deee
James Reeves <jreeves@weavejester.com>
2016-04-13 00:19:20 +0100

    Fill in project descriptions and URLs

--- hop.core/project.clj
+++ hop.core/project.clj
@@ -1,6 +1,6 @@
 (defproject hop/hop.core "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An experimental declarative build tool"
+  :url "https://github.com/weavejester/hop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
--- hop.lein/project.clj
+++ hop.lein/project.clj
@@ -1,6 +1,6 @@
 (defproject hop/hop.lein "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen integration task for Hop"
+  :url "https://github.com/weavejester/hop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
