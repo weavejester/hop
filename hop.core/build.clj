eed875cbdf76b6b62028a5ec9ccfe16936b9d45e
James Reeves <jreeves@weavejester.com>
2016-04-12 14:53:40 +0100

    Move Leiningen task into own project
    This ensures other tasks don't need all Leiningen's dependencies.

--- hop.core/build.clj
+++ hop.core/build.clj
@@ -1,9 +1,6 @@
 {:name "hop"
  :group "hop"
  :version "0.1.0-SNAPSHOT"
- :dependencies [[org.clojure/clojure "1.8.0"]
-                [com.cemerick/pomegranate "0.3.0"]
-                [meta-merge "0.1.1"]
-                [medley "0.7.3"]]
- :tasks {"refresh" {:main hop.task.core}
-         "lein"    {:main hop.task.leiningen, :args [~task], :aot :all}}}
+ :tasks {"lein" {:dependencies [[hop/hop.lein "0.1.0-SNAPSHOT"]]
+                 :main hop.task.leiningen
+                 :args [~task]}}}
