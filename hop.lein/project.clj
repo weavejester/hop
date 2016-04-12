7383ef14022a7b9cb39f878d62da2418ce55d74b
James Reeves <jreeves@weavejester.com>
2016-04-12 16:54:56 +0100

    Release 0.0.1

--- hop.lein/project.clj
+++ hop.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject hop/hop.lein "0.1.0-SNAPSHOT"
+(defproject hop/hop.lein "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
